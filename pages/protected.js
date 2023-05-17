814679c7a8d51c8ebd2bfd749d9d2d292efa1c3b
unknown <sean.jin@teehub1.com>
2023-05-17 15:50:54 -0700

    bugfix

---
+++ pages/protected.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { withSSRContext } from 'aws-amplify';
+
+function Protected({authenticated, username}){
+    if(!authenticated){
+        return <h1>Not authenticated</h1>
+    }
+    return <h1>Hello {userName} from SSR Route</h1>
+}
+
+export async function getServerSideProps(context){
+    const { Auth } = withSSRContext(context)
+    try {
+        const user = await Auth.currentAuthenticatedUser()
+        return {
+            props:{
+                authenticated: true, username:user.username
+            }
+        }
+    }
+}
